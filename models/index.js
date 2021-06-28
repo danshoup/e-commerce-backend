577bfb9437f6bb490a60ada5e4f2e319db20d759
Daniel Shoup <79939129+danshoup@users.noreply.github.com>
2021-06-28 17:31:38 -0400

    built out models

--- models/index.js
+++ models/index.js
@@ -5,13 +5,13 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+  // belongsTo here...
 // Categories have many Products
-
+  // hasMany here...
 // Products belongToMany Tags (through ProductTag)
-
+  // belongsToMany here...
 // Tags belongToMany Products (through ProductTag)
-
+  // belongsToMany here...
 module.exports = {
   Product,
   Category,
